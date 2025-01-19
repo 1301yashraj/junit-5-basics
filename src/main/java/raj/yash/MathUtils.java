654d6c0c735deeb8b8e853678c9deeea178c0d79
1301yashraj <1301yashraj@gmail.com>
2025-01-19 10:35:18 +0530

    Project Setup and Creating a Test class

---
+++ src/main/java/raj/yash/MathUtils.java
@@ -0,0 +1,8 @@
+package raj.yash;
+
+public class MathUtils {
+
+    public int add(int a1, int a2) {
+        return a1 + a2;
+    }
+}
---
+++ src/test/java/raj/yash/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package raj.yash; //usually same for organization conviniance
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This is the Test");
+    }
+
+    
+}
