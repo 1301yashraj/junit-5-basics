de1a6b29a0f919f394b1cb2505efbcfd0f47137c
1301yashraj <1301yashraj@gmail.com>
2025-01-20 18:02:27 +0530

    Basic Asserts

--- src/main/java/raj/yash/MathUtils.java
+++ src/main/java/raj/yash/MathUtils.java
@@ -5,4 +5,21 @@ public class MathUtils {
     public int add(int a1, int a2) {
         return a1 + a2;
     }
+
+    public int substract(int a, int b){
+        return a-b;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a,int b){
+        return a/b;
+    }
+
+    public double area(int radius) {
+        //did multiply by 100 to round up to 2 decimal places
+        return Math.round(Math.PI * radius * radius * 100.0) / 100.0;
+    }
 }
--- src/test/java/raj/yash/MathUtilsTest.java
+++ src/test/java/raj/yash/MathUtilsTest.java
@@ -1,13 +1,51 @@
 package raj.yash; //usually same for organization conviniance
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
-    @Test
-    void test() {
-        System.out.println("This is the Test");
+    @Test // tells junit this method to run
+    void testAdd() {
+        MathUtils mathUntils = new MathUtils();
+        int expectedValue = 3;
+        int actualValue = mathUntils.add(1, 2);
+        assertEquals(expectedValue, actualValue, "Add two numbers");
     }
 
-    
+    // when you run mvn test junit platform runs all the @Test annoted methods.
+    // No news is good News >> No failure means success.
+    // asserrtions are used to assert that something has happend as we wanted it to.
+
+    // Other methods :
+    // assertEquals(expected, actual)
+    // assertArrayEquals(expectedArray, actualArray)
+    // assertIterableEquals(expectedArray, actualArray)
+
+    // @Test
+    // void testDivide1() {
+    // MathUtils mathUtils = new MathUtils();
+    // try {
+    // mathUtils.divide(1, 0);
+    // } catch (ClassCastException c) {
+
+    // }
+    // fail("SOme other Type of exception");
+    // }
+
+    @Test
+    void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0));
+    }
+
+    @Test
+    void testAreaCircle() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.16, mathUtils.area(10), "Area of Circle");
+    }
+
 }
